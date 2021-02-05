db3bc2750f8c5b6962bc4846be6c4d1722358291
hatomachikadai <e195728@ie.u-ryukyu.ac.jp>
2021-02-05 12:12:09 +0900

    ステップ3

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,13 @@
 public class Main{
     public static void main(String[] args){
-        try{
-        String str = null;
-        System.out.println(str.length());
-    }catch(NullPointerException e){
-        System.out.println("nullが入力されている");
-        System.out.println(e.getMessage());
-    }
+        //try{
+        //String str = null;
+        //System.out.println(str.length());
+    //}catch(NullPointerException e){
+      //  System.out.println("nullが入力されている");
+        //System.out.println(e.getMessage());
+    //}
+    String str = "百二十三";
+    int value = Integer.parseInt(str);
 }
 }
