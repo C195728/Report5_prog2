456b3b134c344004c84529ab10c40d0b77892e3b
hatomachikadai <e195728@ie.u-ryukyu.ac.jp>
2021-02-05 12:07:43 +0900

    ステップ2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main{
     public static void main(String[] args){
+        try{
         String str = null;
         System.out.println(str.length());
+    }catch(NullPointerException e){
+        System.out.println("nullが入力されている");
+        System.out.println(e.getMessage());
     }
+}
 }
